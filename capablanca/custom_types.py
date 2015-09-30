7dc5fbaedac076592299e4f77f2d544bfabff164
Dario Blanco <dario.blanco@wywy.com>
2015-09-30 15:03:14 +0200

    Retrieve console parameters using click library

---
+++ capablanca/custom_types.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015, Dario Blanco
+
+import click
+
+
+class BoardDimension(click.ParamType):
+    name = 'dimension'
+
+    def convert(self, value, param, ctx):
+        """Returns a dimension (widht/height) within the allowed bounds"""
+
+        try:
+            dimension = int(value)
+        except ValueError:
+            self.fail('{} is not a valid integer'.format(dimension))
+        else:
+            if 0 < dimension < 100:
+                return dimension
+            else:
+                self.fail('{} should be greater than 0 and lesser than 100'
+                          ''.format(dimension))
+
+    def __repr__(self):
+        return 'DIMENSION'
+
+
+class PieceNumber(click.ParamType):
+    name = 'piece_number'
+
+    def convert(self, value, param, ctx):
+        """Returns the number of pieces if it fits in the dimension"""
+
+        try:
+            n_pieces = int(value)
+        except ValueError:
+            self.fail('{} is not a valid integer'.format(n_pieces))
+        else:
+            if n_pieces < 1000:
+                return n_pieces
+            else:
+                self.fail('{} should be lesser than 1000'.format(n_pieces))
+
+    def __repr__(self):
+        return 'PIECE_NUMBER'
---
+++ capablanca/play.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015, Dario Blanco
+
+import click
+
+from capablanca import custom_types
+
+
+@click.command()
+def play():
+    """
+    Asks interactively for user chess parameters and calculates all possible
+    chess boards where none of the placed pieces can take any of the others
+    """
+    width = click.prompt("Please enter board width",
+                         type=custom_types.BoardDimension())
+    height = click.prompt("Please enter board heigth",
+                          type=custom_types.BoardDimension())
+
+    pieces = {
+        'King': 0,
+        'Queen': 0,
+        'Bishop': 0,
+        'Rook': 0,
+        'Knight': 0
+    }
+    pieces['King'] = click.prompt("Please enter number of Kings",
+                                  type=custom_types.PieceNumber())
+    pieces['Queen'] = click.prompt("Please enter number of Queens",
+                                   type=custom_types.PieceNumber())
+    pieces['Bishop'] = click.prompt("Please enter number of Bishops",
+                                    type=custom_types.PieceNumber())
+    pieces['Rook'] = click.prompt("Please enter number of Rooks",
+                                  type=custom_types.PieceNumber())
+    pieces['Knight'] = click.prompt("Please enter number of Knights",
+                                    type=custom_types.PieceNumber())
+
+
+if __name__ == '__main__':
+    play()
