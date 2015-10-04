48f6c878896feb9c8464ec8f5ee33fa30029f203
Dario Blanco <dario.blanco@wywy.com>
2015-10-04 11:29:23 +0200

    Add integration test in a 7x7 board with a big number of solutions

--- test/test_capablanca.py
+++ test/test_capablanca.py
@@ -12,3 +12,14 @@ def test_8queens_8x8(runner):
     result = runner.invoke(play, input='8\n8\n0\n8\n0\n0\n0\n')
 
     assert '92 solutions found' in result.output
+
+
+@pytest.mark.testtype("integration")
+def test_2k_2q_2b_1n_7x7(runner):
+    """
+    Should run a game with 2 Kings, 2 Queens, 2 Bishop and 1 Knight
+    in a 7x7 board
+    """
+    result = runner.invoke(play, input='7\n7\n2\n2\n2\n0\n1\n')
+
+    assert '3063828 solutions found' in result.output
